b0a4a4944de0ba4825d7d4d8d7a833810079705f
Jeon Chan Min <chanmin010702@gmail.com>
2025-05-13 18:18:13 +0900

    feat: 객체 설계를 테이블 설계에 맞춘 방식 - member, team

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,3 +1,5 @@
+package hellojpa;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -13,6 +15,22 @@ public class JpaMain {
 		tx.begin();
 
 		try {
+
+			Team team = new Team();
+			team.setName("TeamA");
+			em.persist(team);
+
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setTeamId(team.getId());
+			em.persist(member);
+
+			Member findMember = em.find(Member.class, member.getId());
+
+			Long findTeamId = findMember.getTeamId();
+			Team findTeam = em.find(Team.class, findTeamId);
+
+			tx.commit();
 
 			// IDENTITY 전략은 em.persist() 시점에 즉시
 			// INSERT SQL을 실행하고 DB에서 식별자를 조회
@@ -46,7 +64,6 @@ public class JpaMain {
 			tx.commit();
 			*/
 
-			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,132 +1,144 @@
-// package hellojpa;
-//
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-//
-// @Entity
-// // @TableGenerator(
-// // 	name = "MEMBER_SEQ_GENERATOR",
-// // 	table = "MY_SEQUENCES",
-// // 	pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
-// public class Member {
-// 	@Id
-// 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-// 	private Long id;
-//
-// 	@Column(name = "name", nullable = false)
-// 	private String username;
-//
-// 	protected Member() {
-//
-// 	}
-//
-// 	public Long getId() {
-// 		return id;
-// 	}
-//
-// 	public void setId(Long id) {
-// 		this.id = id;
-// 	}
-//
-// 	public String getUsername() {
-// 		return username;
-// 	}
-//
-// 	public void setUsername(String username) {
-// 		this.username = username;
-// 	}
-// 	// 필드와 컬럼 매핑
-// 	/*
-// 	private int age;
-//
-// 	@Enumerated(EnumType.STRING)
-// 	private RoleType roleType;
-//
-// 	@Temporal(TemporalType.TIMESTAMP)
-// 	private Date createdDate;
-//
-// 	private LocalDate testLocalDate;
-// 	private LocalDateTime testLocalDateTime;
-//
-// 	@Temporal(TemporalType.TIMESTAMP)
-// 	private Date lastModifinedDate;
-//
-// 	@Lob
-// 	private String description;
-//
-// 	@Transient
-// 	private int temp;
-//
-// 	protected Member() {
-//
-// 	}
-//
-// 	public Long getId() {
-// 		return id;
-// 	}
-//
-// 	public void setId(Long id) {
-// 		this.id = id;
-// 	}
-//
-// 	public String getUsername() {
-// 		return username;
-// 	}
-//
-// 	public void setUsername(String username) {
-// 		this.username = username;
-// 	}
-//
-// 	public int getAge() {
-// 		return age;
-// 	}
-//
-// 	public void setAge(int age) {
-// 		this.age = age;
-// 	}
-//
-// 	public RoleType getRoleType() {
-// 		return roleType;
-// 	}
-//
-// 	public void setRoleType(RoleType roleType) {
-// 		this.roleType = roleType;
-// 	}
-//
-// 	public Date getCreatedDate() {
-// 		return createdDate;
-// 	}
-//
-// 	public void setCreatedDate(Date createdDate) {
-// 		this.createdDate = createdDate;
-// 	}
-//
-// 	public Date getLastModifinedDate() {
-// 		return lastModifinedDate;
-// 	}
-//
-// 	public void setLastModifinedDate(Date lastModifinedDate) {
-// 		this.lastModifinedDate = lastModifinedDate;
-// 	}
-//
-// 	public String getDescription() {
-// 		return description;
-// 	}
-//
-// 	public void setDescription(String description) {
-// 		this.description = description;
-// 	}
-//
-// 	public int getTemp() {
-// 		return temp;
-// 	}
-//
-// 	public void setTemp(int temp) {
-// 		this.temp = temp;
-// 	}
-// 	*/
-//
-// }
+package hellojpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+// @TableGenerator(
+// 	name = "MEMBER_SEQ_GENERATOR",
+// 	table = "MY_SEQUENCES",
+// 	pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+public class Member {
+	@Id
+	@GeneratedValue
+	@Column(name = "MEMBER_ID")
+	private Long id;
+
+	@Column(name = "USERNAME")
+	private String username;
+
+	@Column(name = "TEAM_ID")
+	private Long teamId;
+
+	protected Member() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getTeamId() {
+		return teamId;
+	}
+
+	public void setTeamId(Long teamId) {
+		this.teamId = teamId;
+	}
+
+	// 필드와 컬럼 매핑
+	/*
+	private int age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	private LocalDate testLocalDate;
+	private LocalDateTime testLocalDateTime;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifinedDate;
+
+	@Lob
+	private String description;
+
+	@Transient
+	private int temp;
+
+	protected Member() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifinedDate() {
+		return lastModifinedDate;
+	}
+
+	public void setLastModifinedDate(Date lastModifinedDate) {
+		this.lastModifinedDate = lastModifinedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getTemp() {
+		return temp;
+	}
+
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+	*/
+
+}
